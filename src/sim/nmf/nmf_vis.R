0086295fe69945dfb9dfe10236dc95aab203dab4
Kris Sankaran <sankaran.kris@gmail.com>
2017-01-26 15:39:42 -0800

    Directly visualize errors in dimensions

--- src/sim/nmf/nmf_vis.R
+++ src/sim/nmf/nmf_vis.R
@@ -29,7 +29,7 @@ theta_fits <- reshape_all_samples(
 )
 theta_fits$method <- basename(as.character(theta_fits$method))
 
-## ---- visualize-thetas -----
+## ---- visualizethetas-prep -----
 ## Visualize the fitted thetas, according to a few different simulation properties
 plot_opts <- list(
   "x" = "value_1",
@@ -41,10 +41,10 @@ plot_opts <- list(
   "alpha" = 0.05,
   "h" = 0.1,
   "mean_col" = "#e34a33",
-  "x_lim" = c(0, 3.5),
-  "y_lim" = c(0, 5.4),
+  "x_lim" = c(0, 4),
+  "y_lim" = c(0, 5),
   "text_size" = 2,
-  "panel_border" = 0.2
+  "panel_border" = 0.1
 )
 
 ## first, visualization in the non-zero-inflated case
@@ -52,7 +52,35 @@ gamma_pois_data <- theta_fits %>%
   filter(zero_inf_prob == 0, method == "nmf_gamma_poisson.stan")
 
 theta_plots <- scores_contours(gamma_pois_data, plot_opts)
-theta_plots$grouped
+
+## ---- visualizethetas ----
+#theta_plots$grouped +
+  labs(
+    "x" = expression(theta[1]),
+    "y" = expression(theta[2])
+  )
+
+## ---- visualizethetashist ----
+theme_set(min_theme(list(border_size = 0.5)))
+mgamma_pois_data <- gamma_pois_data %>%
+  melt(
+    variable.name = "dimension",
+    value.name = "estimate",
+    measure.vars = c("value_1", "value_2")
+  ) %>%
+  melt(
+    variable.name = "truth_dimension",
+    measure.vars = c("truth_1", "truth_2"),
+    value.name = "truth"
+  )
+
+ggplot(mgamma_pois_data) +
+  geom_histogram(
+    aes(x = sqrt(truth) - sqrt(estimate), fill = dimension),
+    position = "identity", alpha = 0.7, bins = 75
+  ) +
+  facet_grid(formula(paste(plot_opts$facet_terms, collapse = "~"))) +
+  scale_fill_manual(values = c("#d95f02", "#7570b3"))
 
 ## ---- visualize-zinf-thetas ----
 zinf_data <- theta_fits %>%
