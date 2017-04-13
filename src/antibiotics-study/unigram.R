1bf4750b733f67850f19fb51bf8f98a6d4fb3f2c
Kris Sankaran <sankaran.kris@gmail.com>
2017-04-13 14:29:26 -0700

    Some refinements to unigram figure

--- src/antibiotics-study/unigram.R
+++ src/antibiotics-study/unigram.R
@@ -83,7 +83,18 @@ mu_hat <- mu_hat %>%
   left_join(taxa) %>%
   left_join(sample_data(abt)[, c("time", "condition")]) %>%
   group_by(time) %>%
-  mutate(prob = softmax(mu))
+  mutate(
+    prob = softmax(mu),
+    condition = revalue(
+      condition,
+      c("Pre Cp" = "Pre",
+        "1st Cp" = "1st Course",
+        "1st WPC" = "1st Course",
+        "2nd Cp" = "2nd Course",
+        "2nd WPC" = "2nd Course",
+        "Post Cp" = "Post")
+    )
+  )
 
 group_order <- sort(table(taxa$Taxon_5), decreasing = TRUE)
 mu_hat$Taxon_5 <- factor(mu_hat$Taxon_5, levels = names(group_order))
@@ -127,13 +138,13 @@ plot_opts <- list(
   "alpha" = 0.4,
   "col_colors" = brewer.pal(8, "Set2"),
   "fill_colors" = brewer.pal(8, "Set2"),
-  "theme_opts" = list(border_size = 0.7)
+  "theme_opts" = list(border_size = 0.7, text_size = 10, subtitle_size = 10)
 )
 p <- ggboxplot(
   mu_hat %>%
   filter(
     Taxon_5 %in% levels(mu_hat$Taxon_5)[1:4],
-    time %in% seq(10, 20, by = 3)
+    time %in% seq(10, 26, by = 5)
   ) %>%
   as.data.frame(),
   plot_opts
@@ -141,12 +152,14 @@ p <- ggboxplot(
   geom_hline(yintercept = 0, alpha = 0.4, size = 0.5, col = "#999999") +
   scale_y_continuous(breaks = scales::pretty_breaks(3)) +
   facet_grid(condition + time ~ Taxon_5, scales = "free_x", space = "free_x") +
+  labs(y = expression(mu[t]), fill = "Taxonomic Family") +
   theme(
     strip.text.x = element_blank(),
     axis.text.x = element_blank(),
-    legend.position = "bottom"
+    axis.title = element_text(size = 11),
+    legend.position = "bottom",
   )
-ggsave("../../doc/figure/unigramboxplots-1.pdf", p)
+ggsave("../../doc/figure/antibiotics_unigram_mu.pdf", p, width = 6, height = 3.8)
 
 ## ---- posterior-checks ----
 counts_data_checker(x, samples$x_sim, "../../doc/figure/unigram_post_checks")
